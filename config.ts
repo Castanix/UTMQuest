ff639855d2e443b0c28dfe3fb86be1af3e7d8acb
Castanix <32367135+Castanix@users.noreply.github.com>
2022-09-27 21:53:39 -0400

    14-add-mongo-collections (#22)
    * Setup mongoDB and add collections and validations
    
    * Minor documentation, fixes, and tests
    
    * convert js to ts
    
    * convert js to ts
    
    Co-authored-by: Chris Wan <chrisxiranwan@gmail.com>

---
+++ config.ts
@@ -0,0 +1,9 @@
+import * as dotenv from 'dotenv';
+dotenv.config({path:__dirname+'/.env'})
+
+const configValues = { 
+    MONGO_URI: process.env.MONGO_URI ?? '',
+    DB_NAME: process.env.DB_NAME ?? '',
+}
+
+export default configValues;
