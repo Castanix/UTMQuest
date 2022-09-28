1a5a2942a424e884d9d497e2afe997bc66dd87bc
Chris Wan <chrisxiranwan@gmail.com>
2022-09-27 21:51:09 -0400

    convert js to ts

--- backend/server.ts
+++ backend/server.ts
@@ -1,35 +1,17 @@
-import express, { Request, Response } from "express"; 
-import * as mongoDB from "mongodb";
-import connectDB from './db/db';
+import express, { Request, Response } from "express";
+import { connectDB } from "./db/db.service"
 
 const app = express();
-const port = process.env.PORT || 5000;
-
-let db;
-connectDB().then((database:mongoDB.Db) => {
-    db = database;
-
-
-    // Insert test for invalid document to Accounts collection
-    // db.collection("Accounts").insertOne({
-    //   utorid: "test123",
-    //   utorName: "testName",
-    //   savedCourses: ["testItem", "testItem2"],
-    //   reviewQns: "wrong bsonType"
-    // })
-
-
-    // Insert test for valid document to Accounts collection
-    db.collection("Accounts").insertOne({
-      utorid: "test123",
-      utorName: "testName",
-      savedCourses: ["testItem", "testItem2", "testing empty array in reviewQns"],
-      reviewQns: []
-    })
-})
-
-app.listen(port, () => console.log(`Listening on port ${port}`));
+const port = process.env.PORT || 5001;
 
 app.get('/express_backend', (req: Request, res: Response) => {
   res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' });
 });
+
+connectDB().then(async (collection) => {
+  app.listen(port, () => console.log(`Listening on port ${port}`));  
+
+
+}).catch((error: Error) => {
+  console.error(`Error could not connect to db: ${error}`);
+})
