9a8cd067b12274d27302b0156ad7e4a71f1b71a2
chriswan12 <71847150+chriswan12@users.noreply.github.com>
2022-10-02 18:35:41 -0400

    Finish issue 16 - Courses API: implement routes required by the cours… (#26)
    * Finish issue 16 - Courses API: implement routes required by the courses api
    
    * change uri name from allCourses to getAllCourses
    
    * Update coursePage.tsx
    
    remove console.log

---
+++ backend/routes/Courses.ts
@@ -0,0 +1,57 @@
+import { Request, Response } from "express";
+import { Router } from "express";
+import { collections } from "../server";
+
+const courseRouter = Router();
+
+courseRouter.get('/getAllCourses', async (req: Request, res: Response) => { 
+    try { 
+        let courseLst = await collections.Courses?.find().toArray();
+        res.json(courseLst);
+    } catch (error) { 
+        res.status(500).send("ERROR: " + error);
+    }
+})
+
+courseRouter.get('/getCourse/:courseId', (req: Request, res: Response) => {
+    collections.Courses?.findOne({courseId: req.params.courseId}).then((doc: any) => {
+
+    if (doc == null) {
+      // set custom statusText to be displayed to user
+      res.statusMessage = "No such course found."
+      res.status(404).end();
+    }
+
+    else { 
+      res.status(200).send(doc);
+    }
+
+    }).catch((error) => {
+      res.status(500).send("ERROR: " + error);
+    })
+})
+
+courseRouter.post('/', async (req: Request, res: Response) => {
+    const course = {
+      courseId: req.body.courseId,
+      courseName: req.body.courseName,
+      topics: req.body.courseTopics
+    }  
+
+    const item = await collections.Courses?.findOne(course); 
+    if (item){ 
+      res.status(409).send({ "error": "data already exists"}); 
+      return;
+    }
+    
+    collections.Courses?.insertOne(course).then((result) => { 
+    if (!result){ 
+      res.status(400).send("Unable to post the course");
+    }
+      res.status(201).send(`course ${course.courseId} has been added succesfully`); 
+    }).catch((error) => { 
+      res.status(500).send("ERROR: " + error)
+    })
+})
+
+export { courseRouter };
