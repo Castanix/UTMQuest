d77f5463c9fb8ad4ce760a0ab5d68a34a350ff17
chriswan12 <71847150+chriswan12@users.noreply.github.com>
2022-10-19 16:26:44 -0400

    Timetable api issue 27 (#44)
    * add script to fetch UTM timetable API to fetch UTM courses and store them into the database
    
    * fix merge conflicts with dev
    
    * remove topics

---
+++ backend/fetchUTMCourses.ts
@@ -0,0 +1,40 @@
+import axios from "axios";
+import { utmQuestCollections } from "./db/db.service";
+
+const dataBody = {
+  courseCodeAndTitleProps: { "courseCode": "", "courseTitle": "", "courseSectionCode": "" },
+  courseLevels: [],
+  creditWeights: [],
+  dayPreferences: [],
+  deliveryModes: [],
+  departmentProps: [],
+  direction: "asc",
+  divisions: ["ERIN"],
+  instructor: "",
+  page: 1,
+  pageSize: 2000,
+  requirementProps: [],
+  sessions: ["20229", "20231", "20229-20231"],
+  timePreferences: []
+}
+
+axios({
+  method: 'post',
+  url: 'https://api.easi.utoronto.ca/ttb/getPageableCourses',
+  data: dataBody,
+}).then((result) => {
+  Object.values(result.data.payload.pageableCourse.courses).forEach( (item: any) => { 
+    const course = {
+        courseId: item.code,
+        courseName: item.name
+    }
+    utmQuestCollections.Courses?.insertOne(course).then((res) => {
+        if (!res) { 
+            console.log(`INSERTION FOR COURSE ${course} HAS FAILED!`);
+        }
+        console.log("SUCCESSFULLY INSERTED COURSES"); 
+    }).catch((err) => { 
+        console.log(err);
+    })
+  });
+});
