23adcf04ad00cf4625c57ab40540810396458af2
chriswan12 <71847150+chriswan12@users.noreply.github.com>
2022-11-04 14:01:09 -0400

    Discussion page - populate page with real data (#77)
    * feat: add discussion page with mocked comments
    
    * made changes to backend discussion API, database initalization and types
    
    Co-authored-by: Dhaval <dhaval.malhotra@mail.utoronto.ca>

---
+++ src/components/Discussion/fetch/GetChildComments.tsx
@@ -0,0 +1,30 @@
+import { message } from 'antd';
+import React from 'react';
+import { DiscussionFrontEndType } from "../../../../backend/types/Discussion";
+
+const GetChildComments = (commentIds: string[], setChildComments: React.Dispatch<React.SetStateAction<DiscussionFrontEndType[]>>) => {
+
+    const loadingMessage = message.loading('Loading comments', 0);
+    
+    let lstOfIds = ''; 
+    for (let i = 0; i < commentIds.length; i++) { 
+        lstOfIds = lstOfIds.concat(`id${i}=${commentIds[i]}&`);
+    }
+
+    fetch(
+        `${process.env.REACT_APP_API_URI}/discussion/allThreads/ids?${lstOfIds.slice(0,-1)}`
+    )
+        .then((res: Response) => {
+            if (!res.ok) throw Error(res.statusText);
+            return res.json();
+        })
+        .then((result) => {
+            setChildComments(result);
+            setTimeout(loadingMessage, 1000);
+        })
+        .catch((error) => {
+            message.error(error);
+        });
+};
+
+export default GetChildComments;
--- src/pages/ApprovedQuestion/ApprovedQuestion.tsx
+++ src/pages/ApprovedQuestion/ApprovedQuestion.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import './ApprovedQuestion.css'; 
+import './ApprovedQuestion.css';
 import MDEditor from '@uiw/react-md-editor';
 import { Breadcrumb, Button, Card } from 'antd';
 import { Link, useParams } from 'react-router-dom';
@@ -13,26 +13,27 @@ import { TypeOfQuestion } from './types/QuestionTypes';
 import { QuestionsType } from "../../../backend/types/Questions";
 import ShortAnswer from "../../components/ShortAnswer/ShortAnswer";
 import GetQuestion from "./fetch/GetQuestion";
+import Discussion from "../../components/Discussion/Discussion";
 
 
-const QuestionTab = ({options, answers, actualQuestion, explanation}: 
-    {options: string[], answers: string[], actualQuestion: string, explanation: string}) => ( 
-    <div> 
-        <Title level={3} className='divider-title'>Problem</Title>
-        <MDEditor.Markdown source={actualQuestion} />
-        <br/>
-        <br/>
-        <Title level={3} className='divider-title'>Your answer</Title>
-        <MultipleChoice options={options} answers={answers} explanation={explanation}/>
-    </div>
-);
-
-const ShortAnswerTab = ({actualQuestion, answer}: {actualQuestion: string, answer: string}) => (
+const QuestionTab = ({ options, answers, actualQuestion, explanation }:
+    { options: string[], answers: string[], actualQuestion: string, explanation: string }) => (
     <div>
         <Title level={3} className='divider-title'>Problem</Title>
         <MDEditor.Markdown source={actualQuestion} />
-        <br/>
-        <br/>
+        <br />
+        <br />
+        <Title level={3} className='divider-title'>Your answer</Title>
+        <MultipleChoice options={options} answers={answers} explanation={explanation} />
+    </div>
+);
+
+const ShortAnswerTab = ({ actualQuestion, answer }: { actualQuestion: string, answer: string }) => (
+    <div>
+        <Title level={3} className='divider-title'>Problem</Title>
+        <MDEditor.Markdown source={actualQuestion} />
+        <br />
+        <br />
         <Title level={3} className='divider-title'>Your answer</Title>
         <ShortAnswer answer={answer} />
     </div>
@@ -40,41 +41,41 @@ const ShortAnswerTab = ({actualQuestion, answer}: {actualQuestion: string, answe
 
 const Header = ({ courseCode, courseTitle, approved, questionName, topicName, author, date }:
     { courseCode: string, courseTitle: string, approved: boolean, questionName: string, topicName: string, author: string, date: string }) => (
-        <div>
-            <Breadcrumb>
-                <Breadcrumb.Item><Link to="/">Dashboard</Link></Breadcrumb.Item>
-                <Breadcrumb.Item><Link to="/courses">Courses</Link></Breadcrumb.Item>
-                <Breadcrumb.Item><Link to={`/courses/${courseCode}`}>{courseCode}</Link></Breadcrumb.Item>
-                { approved ? 
-                    <Breadcrumb.Item><Link to={`/courses/${courseCode}/browse`}>Browse Questions</Link></Breadcrumb.Item> :
-                    <Breadcrumb.Item>Review Questions</Breadcrumb.Item> }
-                <Breadcrumb.Item>{questionName}</Breadcrumb.Item>
-            </Breadcrumb>
-            <div className="title">
-                <div className="title-flex">
-                    <Title level={3} ellipsis>{courseTitle} <div className="subtitle">&#8226; {topicName}</div></Title>
-                    <Text type="secondary">{`by ${author} on ${new Date(date).toDateString()}`}</Text>
+    <div>
+        <Breadcrumb>
+            <Breadcrumb.Item><Link to="/">Dashboard</Link></Breadcrumb.Item>
+            <Breadcrumb.Item><Link to="/courses">Courses</Link></Breadcrumb.Item>
+            <Breadcrumb.Item><Link to={`/courses/${courseCode}`}>{courseCode}</Link></Breadcrumb.Item>
+            {approved ?
+                <Breadcrumb.Item><Link to={`/courses/${courseCode}/browse`}>Browse Questions</Link></Breadcrumb.Item> :
+                <Breadcrumb.Item>Review Questions</Breadcrumb.Item>}
+            <Breadcrumb.Item>{questionName}</Breadcrumb.Item>
+        </Breadcrumb>
+        <div className="title">
+            <div className="title-flex">
+                <Title level={3} ellipsis>{courseTitle} <div className="subtitle">&#8226; {topicName}</div></Title>
+                <Text type="secondary">{`by ${author} on ${new Date(date).toDateString()}`}</Text>
+            </div>
+            <div className="icon-buttons">
+                <div className="flex-child">
+                    <Button type="link" icon={<EditTwoTone style={{ fontSize: '1.35rem', alignItems: 'center' }} />} />
+                    <p className="icon-text">Edit</p>
                 </div>
-                <div className="icon-buttons"> 
-                    <div className="flex-child"> 
-                        <Button type="link" icon={<EditTwoTone style={{ fontSize: '1.35rem', alignItems: 'center'}}/>} />
-                        <p className="icon-text">Edit</p>
-                    </div>
-                    <div className="flex-child">
-                        <Button type="link" danger icon={<WarningFilled style={{ fontSize: '1.35rem', alignItems: 'center'}}/>} />
-                        <p className="icon-text report-text">Report</p>
-                    </div>
+                <div className="flex-child">
+                    <Button type="link" danger icon={<WarningFilled style={{ fontSize: '1.35rem', alignItems: 'center' }} />} />
+                    <p className="icon-text report-text">Report</p>
                 </div>
             </div>
         </div>
-    );
+    </div>
+);
 
-const getQuestionType = (question: QuestionsType) => ({ 
-    mc: <QuestionTab options={question.choices} answers={question.ans as string[]} actualQuestion={question.desc} explanation={question.xplan}/>,
+const getQuestionType = (question: QuestionsType) => ({
+    mc: <QuestionTab options={question.choices} answers={question.ans as string[]} actualQuestion={question.desc} explanation={question.xplan} />,
     short: <ShortAnswerTab actualQuestion={question.desc} answer={question.ans as string} />
 });
 
-const QuestionType = ({question, qnsType}: {question: QuestionsType, qnsType: keyof TypeOfQuestion}) => <div>{getQuestionType(question)[qnsType]}</div>;
+const QuestionType = ({ question, qnsType }: { question: QuestionsType, qnsType: keyof TypeOfQuestion }) => <div>{getQuestionType(question)[qnsType]}</div>;
 
 const tabList = [
     {
@@ -85,10 +86,10 @@ const tabList = [
         key: 'Discussion',
         tab: 'Discussion',
     },
-];    
+];
 
 const ApprovedQuestion = ({ approved }: { approved: boolean }) => {
-    const [activeTabKey, setActiveTabKey] = useState<string>('Question'); 
+    const [activeTabKey, setActiveTabKey] = useState<string>('Question');
     const params = useParams();
     const courseCode = params.courseId;
     const id = params.id ?? '';
@@ -98,32 +99,32 @@ const ApprovedQuestion = ({ approved }: { approved: boolean }) => {
 
     if (error !== '' || question === undefined) return <ErrorMessage title={error !== '' ? error : 'Could not find question'} link="/courses" message="Go back to courses" />;
 
-    const onTabChange = (key: string) => { 
-        setActiveTabKey(key); 
+    const onTabChange = (key: string) => {
+        setActiveTabKey(key);
     };
 
-    const contentList: Record<string, React.ReactNode> =  { 
-        Question:  <QuestionType question={question} qnsType={question.qnsType as keyof TypeOfQuestion}/>,
-        Solution: <p>Solution</p>, 
-        Discussion: <p>Discussion</p>
+    const contentList: Record<string, React.ReactNode> = {
+        Question: <QuestionType question={question} qnsType={question.qnsType as keyof TypeOfQuestion} />,
+        Solution: <p>Solution</p>,
+        Discussion: <Discussion questionId={question._id} />
     };
 
-    return ( 
-            <Card
-                style={{width: '100%'}}
-                title={<Header courseCode={courseCode ?? ''} courseTitle={`${courseCode} `} approved={approved} 
-                    questionName={question.qnsName} topicName={question.topicName} author={question.authName} date={question.date} />}
-                tabList={tabList}
-                activeTabKey={activeTabKey}
-                onTabChange={key => {
-                    onTabChange(key);
-                }}    
-                bordered={false}
-            >   
-                <main className="main-container">
-                    {contentList[activeTabKey]}
-                </main>
-            </Card>
+    return (
+        <Card
+            style={{ width: '100%' }}
+            title={<Header courseCode={courseCode ?? ''} courseTitle={`${courseCode} `} approved={approved}
+                questionName={question.qnsName} topicName={question.topicName} author={question.authName} date={question.date} />}
+            tabList={tabList}
+            activeTabKey={activeTabKey}
+            onTabChange={key => {
+                onTabChange(key);
+            }}
+            bordered={false}
+        >
+            <main className="main-container">
+                {contentList[activeTabKey]}
+            </main>
+        </Card>
     );
 };
 
