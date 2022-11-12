4402d19514cfd33bb20e5d5b985225c76de16242
Ethan <ethanch007@gmail.com>
2022-11-11 20:30:53 -0500

    Update add question badge to use shared tracker function

--- backend/types/utmQuestCollection.ts
+++ backend/types/utmQuestCollection.ts
@@ -1,7 +1,6 @@
 import * as mongoDB from "mongodb";
 
 export type UTMQuestCollections = {
-<<<<<<< HEAD
 	Accounts?: mongoDB.Collection;
 	Courses?: mongoDB.Collection;
 	Topics?: mongoDB.Collection;
@@ -9,12 +8,3 @@ export type UTMQuestCollections = {
 	Discussions?: mongoDB.Collection;
 	Badges?: mongoDB.Collection;
 };
-=======
-    Accounts?: mongoDB.Collection,
-    Courses?: mongoDB.Collection,
-    Topics?: mongoDB.Collection,
-    Questions?: mongoDB.Collection,
-    Discussions?: mongoDB.Collection,
-    Badges?: mongoDB.Collection,
-}
->>>>>>> Add progressive badge for question edits
