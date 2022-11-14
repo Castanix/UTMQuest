cad8ffed13f25df746ac4992f798dc5fe27fa31f
Dhaval <dhaval.malhotra@mail.utoronto.ca>
2022-11-13 20:12:53 -0500

    fix: linter errors

--- src/components/CourseBoard/CourseBoardTable.tsx
+++ src/components/CourseBoard/CourseBoardTable.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD:src/pages/CourseBoard/CourseBoardTable.tsx
 import React, { useState } from 'react';
 import Table, { ColumnsType } from "antd/lib/table";
 import { Link } from "react-router-dom";
@@ -68,7 +67,7 @@ const CourseBoardTable = (props: any) => {
 
         setTableDisplayData(courses);
         setAllAddedData(courses);
-        
+
     };
 
     return (
@@ -96,101 +95,4 @@ const CourseBoardTable = (props: any) => {
     );
 };
 
-=======
-import React, { useState } from 'react';
-import Table, { ColumnsType } from "antd/lib/table";
-import { Link } from "react-router-dom";
-import { Button, Input } from 'antd';
-import { PlusCircleOutlined, SearchOutlined } from '@ant-design/icons';
-import CoursesType from "../../../backend/types/Courses";
-import AddCourseModal from './AddCourseModal';
-
-const CourseBoardTable = (props: any) => {
-    const { dataSource }: { dataSource: CoursesType[] } = props;
-
-    const added: CoursesType[] = [];
-    const unadded: CoursesType[] = [];
-
-    dataSource.forEach((item) => {
-        if (item.added) {
-            added.push(item);
-        } else {
-            unadded.push(item);
-        }
-    });
-
-    const [allData, setAllData] = useState<CoursesType[]>(added);
-    const [searchValue, setSearchValue] = useState<string>("");
-    const [displayData, setDisplayData] = useState<CoursesType[]>(added);
-    const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
-
-    const columns: ColumnsType<CoursesType> = [{
-        title: "Course Code",
-        dataIndex: "courseId",
-        render: text => <Link to={`/courses/${text}`}>{text}</Link>,
-        sorter: (a: CoursesType, b: CoursesType) => a.courseId.localeCompare(b.courseId),
-        defaultSortOrder: 'ascend'
-    },
-    {
-        title: "Title",
-        dataIndex: "courseName",
-        sorter: (a: CoursesType, b: CoursesType) => a.courseName.localeCompare(b.courseName)
-    },
-    {
-        title: "# of Topics",
-        dataIndex: "numTopics",
-        width: '10%',
-        sorter: (a: CoursesType, b: CoursesType) => a.numTopics - b.numTopics
-    }];
-
-    const handleSearch = (value: string) => {
-        setSearchValue(value);
-        setDisplayData(value ? allData.filter(item =>
-            item.courseId.toLowerCase().includes(value.toLowerCase()) ||
-            item.courseName.toLowerCase().includes(value.toLowerCase())
-        ) : allData);
-    };
-
-    // Locally renders new course on the courseboard. Renders by fetching from db happens on initial page load.
-    const rerender = (code: string, name: string) => {
-        setSearchValue("");
-
-        const courses = [...allData, {
-            _id: "temp",
-            courseId: code,
-            courseName: name,
-            numTopics: 0,
-            added: true
-        }];
-
-        setAllData(courses);
-        setDisplayData(courses);
-    };
-
-    return (
-        <div>
-            <div className='toolbar'>
-                <Input placeholder="Search Course" prefix={<SearchOutlined />} value={searchValue} onChange={(e) => { handleSearch(e.target.value); }} />
-                <Button type="primary" icon={<PlusCircleOutlined />} shape="round" className='add-new-course' onClick={() => { setIsModalOpen(true); }}>
-                    Add a Course
-                </Button>
-            </div>
-            <br />
-            <Table
-                dataSource={displayData}
-                columns={columns}
-                rowKey="_id"
-            />
-            <AddCourseModal
-                modalState={isModalOpen}
-                setModalState={setIsModalOpen}
-                setDisplayData={setDisplayData}
-                rerender={rerender}
-                unadded={unadded}
-            />
-        </div>
-    );
-};
-
->>>>>>> 36b5529 (layout: combine courses page and dashboard page into one):src/components/CourseBoard/CourseBoardTable.tsx
 export default CourseBoardTable;
