018db2be56b0d617afdeaf288dde7407048348fe
Castanix <ethanch007@gmail.com>
2023-01-12 15:40:56 -0500

    Remove old commented code

--- src/components/MultipleChoice/AddMultipleChoice/AddMultipleChoice.tsx
+++ src/components/MultipleChoice/AddMultipleChoice/AddMultipleChoice.tsx
@@ -120,21 +120,6 @@ const AddMultipleChoice = (
                         </Checkbox>
                     }
                 />
-                {// Cleanup once verified working
-                
-                /* {options.map((item, index) =>
-                    <Checkbox key={`checkbox_${item._id}`} onChange={() => onCheckboxChange(index)} checked={options[index].isCorrect}>
-                        <TextArea
-                            className="add-mc-textarea"
-                            key={`input_${item._id}`}
-                            maxLength={1000}
-                            onChange={e => updateOptionValue(index, e.target.value)}
-                            placeholder={`Option ${String.fromCharCode(65 + index)}`}
-                            defaultValue={options[index].value ?? ''}
-                            autoSize
-                        />
-                    </Checkbox>
-                )} */}
             </div>
         </div>
     );
