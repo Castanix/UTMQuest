0f4463bf1e595d462839463bab01a9874dcb82ca
Chris Wan <chrisxiranwan@gmail.com>
2023-01-22 20:54:36 -0500

    convert getAllQuestions into react-query

--- src/pages/ProfilePage/fetch/GetAllQuestions.tsx
+++ src/pages/ProfilePage/fetch/GetAllQuestions.tsx
@@ -1,46 +1,19 @@
-import { useEffect, useState } from "react";
-import { QuestionFrontEndType } from "../../../../backend/types/Questions";
-import { TimelineType } from "../ProfilePage";
+import { useQuery } from "react-query";
 
-const GetAllQuestions = (userId: string, setTimeline: Function) => {
+const fetchAllUserPostedQuestions = async (utorId: string) => {
+    const response = await fetch(`${process.env.REACT_APP_API_URI}/question/allUserPostedQuestions/${utorId}`);
+    if (!response.ok) throw Error(response.statusText);
+    return response.json();
+};
 
-    const [loadingQuestions, setLoadingQuestions] = useState<boolean>(true);
-    const [errorQuestions, setErrorQuestions] = useState('');
+const GetAllQuestions = (userId: string) => {
 
-    useEffect(() => {
-        fetch(`${process.env.REACT_APP_API_URI}/question/allUserPostedQuestions/${userId}`)
-            .then((res: Response) => {
-                if (!res.ok) throw Error(res.statusText);
-                return res.json();
-            }).then((result) => {
-                const timelineArr: TimelineType[] = [];
-
-                result.forEach((question: QuestionFrontEndType) => {
-                    const { courseId, _id: qnsId, qnsLink, qnsName, date } = question;
-
-                    timelineArr.push({
-                        courseId,
-                        qnsId,
-                        qnsLink,
-                        qnsName,
-                        date
-                    });
-                });
-
-                timelineArr.sort((a, b) => Date.parse(b.date) - Date.parse(a.date));
-
-                setTimeline(timelineArr);
-                setLoadingQuestions(false);
-            }).catch((err) => {
-                setErrorQuestions(err.message);
-                setLoadingQuestions(false);
-            });
-
-    }, [setTimeline, userId]);
+    const result = useQuery("userPostedQuestions", () => fetchAllUserPostedQuestions(userId));
 
     return {
-        loadingQuestions,
-        errorQuestions,
+        loadingQuestions: result.isLoading,
+        errorQuestions: result.error,
+        data: result?.data
     };
 };
 
