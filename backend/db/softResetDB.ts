b08544930d57da5e5288e08cb243449e241786bd
Castanix <ethanch007@gmail.com>
2023-01-31 22:09:21 -0500

    Add softResetDB script

---
+++ backend/db/softResetDB.ts
@@ -0,0 +1,35 @@
+import connectDB, { mongoDBConnection, utmQuestCollections } from "./db.service";
+
+
+// Soft resets Courses, Topics, Questions, Discussions (without remaking collections and removing indexes)
+const softResetDB = async () => {
+    await connectDB();
+
+    const session = mongoDBConnection.startSession();
+
+    try {
+        session.startTransaction();
+
+        await utmQuestCollections.Questions?.deleteMany({}, { session });
+        await utmQuestCollections.Discussions?.deleteMany({}, { session });
+        await utmQuestCollections.Topics?.deleteMany({}, { session });
+
+        await utmQuestCollections.Courses?.updateMany(
+            { $or: [{ numTopics: { gt: 0 } }, { added: true }] },
+            { $set: { numTopics: 0, added: false } },
+            { session }
+        );
+
+        await session.commitTransaction();
+
+
+    } catch (err) {
+        console.log("Error soft resetting DB");
+        await session.abortTransaction();
+    } finally {
+        console.log("Completed soft reset");
+        await session.endSession();
+    };
+}
+
+softResetDB();
