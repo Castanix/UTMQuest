2afcb024e1f95d77db27db056c7ac29b3ee97593
Dhaval Malhotra <dhaval.malhotra@mail.utoronto.ca>
2023-03-21 00:01:48 -0400

    fix: relative time calculation

--- src/RelativeTime.ts
+++ src/RelativeTime.ts
@@ -2,26 +2,32 @@ const formatter = new Intl.RelativeTimeFormat(undefined, {
 	numeric: "auto",
 });
 
-const DIVISIONS: any = [
-	{ amount: 60, name: "seconds" },
-	{ amount: 60, name: "minutes" },
-	{ amount: 24, name: "hours" },
-	{ amount: 7, name: "days" },
-	{ amount: 4.34524, name: "weeks" },
-	{ amount: 12, name: "months" },
-	{ amount: Number.POSITIVE_INFINITY, name: "years" },
-];
-
 function GetRelativeTime(date: number) {
-	let duration = (date - new Date().getTime()) / 1000;
+	// @ts-ignore
+	const seconds = Math.floor((new Date() - date) / 1000);
 
-	for (let i = 0; i <= DIVISIONS.length; i++) {
-		const division = DIVISIONS[i];
-		if (Math.abs(duration) < division.amount) {
-			return formatter.format(Math.round(duration), division.name);
-		}
-		duration /= division.amount;
+	let interval = seconds / 31536000;
+
+	if (interval > 1) {
+		return formatter.format(-1 * Math.floor(interval), "years");
 	}
+	interval = seconds / 2592000;
+	if (interval > 1) {
+		return formatter.format(-1 * Math.floor(interval), "months");
+	}
+	interval = seconds / 86400;
+	if (interval > 1) {
+		return formatter.format(-1 * Math.floor(interval), "days");
+	}
+	interval = seconds / 3600;
+	if (interval > 1) {
+		return formatter.format(-1 * Math.floor(interval), "hours");
+	}
+	interval = seconds / 60;
+	if (interval > 1) {
+		return formatter.format(-1 * Math.floor(interval), "minutes");
+	}
+	return formatter.format(-1 * Math.floor(seconds), "seconds");
 }
 
 export default GetRelativeTime;
